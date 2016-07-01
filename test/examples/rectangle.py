467a07c61b1795041530efa468fd62ae544f1fe6
Nico Schlömer <nico.schloemer@gmail.com>
2016-07-01 14:21:57 +0200

    add rectangle example

---
+++ test/examples/rectangle.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Creates a mesh for an ellipsoid.
+'''
+import pygmsh as pg
+
+
+def generate():
+    geom = pg.Geometry()
+
+    geom.add_rectangle(
+            0.0, 1.0,
+            0.0, 1.0,
+            0.0,
+            1.0
+            )
+
+    exit(1)
+
+    return geom
+
+
+if __name__ == '__main__':
+    import meshio
+    points, cells = pg.generate_mesh(generate())
+    meshio.write('rectangle.vtu', points, cells)
