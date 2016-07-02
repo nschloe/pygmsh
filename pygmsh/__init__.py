6a30b23bb1230a949e5650ccf145a01638e94489
Nico Schlömer <nico.schloemer@gmail.com>
2016-07-02 16:23:23 +0200

    version bump

--- pygmsh/__init__.py
+++ pygmsh/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-__version__ = '2.3.0'
+__version__ = '2.4.0'
 __author__ = 'Nico Schlömer'
 __author_email__ = 'nico.schloemer@gmail.com'
 __website__ = 'https://github.com/nschloe/pygmsh'
