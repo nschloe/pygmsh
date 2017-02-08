2c7d0d03009fbb618a1e9027eec5c34c80d4da08
Nico Schlömer <nico.schloemer@gmail.com>
2017-02-08 16:30:45 +0100

    version bump

--- pygmsh/__about__.py
+++ pygmsh/__about__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 
-__version__ = '2.5.1'
+__version__ = '2.6.0'
 __author__ = 'Nico Schlömer'
 __author_email__ = 'nico.schloemer@gmail.com'
 __website__ = 'https://github.com/nschloe/pygmsh'
