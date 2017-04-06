9b59047a2cba029d1d66c9370225f6e6b92d17fd
Nico Schlömer <nico.schloemer@gmail.com>
2017-04-06 11:40:23 +0200

    shorter import in __init__

--- pygmsh/__init__.py
+++ pygmsh/__init__.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 #
-from pygmsh.geometry import Geometry
-from pygmsh.helper import *
-
-from pygmsh.__about__ import (
+from .__about__ import (
         __version__,
         __author__,
         __author_email__,
         __website__,
         )
+
+from .geometry import Geometry
+from .helper import *
+
 
 import pipdated
 if pipdated.needs_checking(__name__):
