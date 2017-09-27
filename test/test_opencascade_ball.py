81d60609c73c35a1a53da3f3536cb4cdecba98c3
Nico Schlömer <nico.schloemer@gmail.com>
2017-09-27 17:34:57 +0200

    opencascade: support extra arguments to ball

--- test/test_opencascade_ball.py
+++ test/test_opencascade_ball.py
@@ -17,9 +17,9 @@ def test():
         characteristic_length_max=0.1,
         )
 
-    geom.add_ball([0.0, 0.0, 0.0], 1.0)
+    geom.add_ball([0.0, 0.0, 0.0], 1.0, x0=-0.9, x1=+0.9, alpha=0.5*pi)
 
-    ref = 4.0/3.0 * pi
+    ref = 0.976088698545
     points, cells, _, _, _ = pygmsh.generate_mesh(geom)
     assert abs(compute_volume(points, cells) - ref) < 1.0e-2 * ref
     return points, cells
