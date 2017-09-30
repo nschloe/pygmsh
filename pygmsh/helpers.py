a216beab4df1a06a4e5ace02026512ca44946d5f
Nico Schlömer <nico.schloemer@gmail.com>
2017-09-30 10:04:39 +0200

    update links in readme to use httpS

--- pygmsh/helpers.py
+++ pygmsh/helpers.py
@@ -127,13 +127,13 @@ def generate_mesh(
             print(line.decode('utf-8'), end='')
 
     p.communicate()
-    assert p.returncode == 0,\
+    assert p.returncode == 0, \
         'Gmsh exited with error (return code {}).'.format(p.returncode)
 
     X, cells, pt_data, cell_data, field_data = meshio.read(msh_filename)
 
     # clean up
-    # os.remove(geo_filename)
+    os.remove(geo_filename)
     os.remove(msh_filename)
 
     # Lloyd smoothing
