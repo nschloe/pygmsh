8f79c2059861423504d3a2f657a7e69aa87cc739
Samuel George Mallinson <sam.mallinson@memjet.com>
2017-12-05 09:40:12 +1100

    added comment # pylint: disable=too-many-branches before function extrude as per suggestion of @nschloe on 2017-08-16

--- pygmsh/geometry.py
+++ pygmsh/geometry.py
@@ -306,6 +306,7 @@ class Geometry(object):
             line_loop, plane_surface
             )
 
+    # pylint: disable=too-many-branches
     def extrude(
             self,
             input_entity,
