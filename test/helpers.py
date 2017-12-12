9a7fdf5b35c06d80aa0a29f6fa2ae895516bca86
Nico Schlömer <nico.schloemer@gmail.com>
2017-12-12 10:54:01 +0100

    more careful volume/area computation

--- test/helpers.py
+++ test/helpers.py
@@ -14,30 +14,29 @@ def prune_nodes(points, cells):
 
 
 def compute_volume(points, cells):
-    # is 3D
-    is_3d = 'tetra' in cells
-
-    # TODO multiple element types (e.g., triangles + quads)
-
-    if is_3d:
-        # tetra
+    if 'tetra' in cells:
+        # 3D: only tetras supported
         mesh = voropy.mesh_tetra.MeshTetra(
                 *prune_nodes(points, cells['tetra'])
                 )
         vol = math.fsum(mesh.cell_volumes)
     else:
-        # triangles
-        mesh = voropy.mesh_tri.MeshTri(*prune_nodes(points, cells['triangle']))
-        vol_tri = math.fsum(mesh.cell_volumes)
-        # quad: treat as two triangles
-        quads = cells['quad'].T
-        split_cells = numpy.column_stack([
-            [quads[0], quads[1], quads[2]],
-            [quads[0], quads[2], quads[3]],
-            ]).T
-        mesh = voropy.mesh_tri.MeshTri(*prune_nodes(points, split_cells))
-        vol_quad = math.fsum(mesh.cell_volumes)
-        vol = vol_tri + vol_quad
+        vol = 0.0
+        if 'triangle' in cells:
+            # triangles
+            mesh = voropy.mesh_tri.MeshTri(
+                    *prune_nodes(points, cells['triangle'])
+                    )
+            vol += math.fsum(mesh.cell_volumes)
+        if 'quad' in cells:
+            # quad: treat as two triangles
+            quads = cells['quad'].T
+            split_cells = numpy.column_stack([
+                [quads[0], quads[1], quads[2]],
+                [quads[0], quads[2], quads[3]],
+                ]).T
+            mesh = voropy.mesh_tri.MeshTri(*prune_nodes(points, split_cells))
+            vol += math.fsum(mesh.cell_volumes)
 
     return vol
 
