dbf43f0707f49a94e3a8ba46196bbf62d6845bd1
Tryfon Antonakakis <tryfon@multiwave.ch>
2018-02-15 22:10:04 +0100

    removed parentheses in assert call

--- test/test_helpers.py
+++ test/test_helpers.py
@@ -1,3 +1,5 @@
+"""Tests module for helpers in tests."""
+
 import numpy as np
 
 import pygmsh
@@ -8,7 +10,7 @@ def test():
     geom = pygmsh.built_in.Geometry()
     geom.add_circle([0, 0, 0], 1, 0.1, make_surface=False)
     points, cells, _, _, _ = pygmsh.generate_mesh(geom)
-    assert(np.abs((compute_volume(points, cells) - 2 * np.pi) / np.pi / 2) < 1e-2)
+    assert np.abs((compute_volume(points, cells) - 2 * np.pi) / np.pi / 2) < 1e-2
 
 
 if __name__ == '__main__':
