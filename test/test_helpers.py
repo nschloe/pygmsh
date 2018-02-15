6026fa1f83143ed305a23598bb85310adca48068
Tryfon Antonakakis <tryfon@multiwave.ch>
2018-02-15 23:50:18 +0100

    reference perimeter stated explicitly

--- test/test_helpers.py
+++ test/test_helpers.py
@@ -10,7 +10,8 @@ def test():
     geom = pygmsh.built_in.Geometry()
     geom.add_circle([0, 0, 0], 1, 0.1, make_surface=False)
     points, cells, _, _, _ = pygmsh.generate_mesh(geom)
-    assert np.abs((compute_volume(points, cells) - 2 * np.pi) / np.pi / 2) < 1e-2
+    ref = 2 * np.pi
+    assert np.abs((compute_volume(points, cells) - ref)) < 1e-2 * ref
 
 
 if __name__ == '__main__':
