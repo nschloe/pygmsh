c39ac001eeb9d530d5f7877ceecbcd0e3fd3f812
Tryfon Antonakakis <tryfon@multiwave.ch>
2018-04-07 22:54:42 +0200

    made VolumeBase inherit from builtin VolumeBase

--- pygmsh/opencascade/volume_base.py
+++ pygmsh/opencascade/volume_base.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 #
+from .. import built_in
 
 
-class VolumeBase(object):
+class VolumeBase(built_in.volume_base.VolumeBase):
     _ID = 0
     dimension = 3
 
     def __init__(self, is_list=False, id0=None):
+        super(VolumeBase, self).__init__()
+
         isinstance(id0, str)
         self.is_list = is_list
         if id0:
