f0b454e038a4cbab95952ed4bb2d8279cac71b67
G. D. McBain <gdmcbain@protonmail.com>
2018-04-30 12:57:34 +1000

    PEP 8 whitespace

--- test/test_circle_transform.py
+++ test/test_circle_transform.py
@@ -20,7 +20,7 @@ def test():
          array([[cos(theta[2]), -sin(theta[2]), 0],
                 [sin(theta[2]), cos(theta[2]), 0],
                 [0, 0, 1]])]
-                   
+
     geom.add_circle(
         [7., 11., 13.],
         1.,
