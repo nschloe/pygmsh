91138edb9c13fea4fffb26d0ce4d0730fc8baeb7
G. D. McBain <gdmcbain@protonmail.com>
2018-09-04 08:35:49 +1000

    fast_conversion=True -> mesh_file_type="vtk" #212

--- test/test_rectangle.py
+++ test/test_rectangle.py
@@ -10,7 +10,7 @@ def test():
     geom.add_rectangle(0.0, 1.0, 0.0, 1.0, 0.0, 0.1)
 
     ref = 1.0
-    points, cells, _, _, _ = pygmsh.generate_mesh(geom, fast_conversion=True)
+    points, cells, _, _, _ = pygmsh.generate_mesh(geom, mesh_file_type="vtk")
     assert abs(compute_volume(points, cells) - ref) < 1.0e-2 * ref
     return points, cells
 
