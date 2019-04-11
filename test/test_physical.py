687aca9f082918f7c007ad70f7bc2b79a77157cc
Antonio Cervone <ant.cervone@gmail.com>
2019-04-11 12:36:07 +0200

    relax check on suitable objects that can have a physical tag.
    The only requirement to add a physical tag is to have an id, so LineBase,
    SurfaceBase and VolumeBase should qualify for it.
    The test case is a pretty common use case in CFD, that fails without this
    patch.

---
+++ test/test_physical.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import pygmsh
+
+
+def test(lcar=0.5):
+    geom = pygmsh.built_in.Geometry()
+    poly = geom.add_polygon(
+        [[0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [1.0, 1.0, 0.0], [0.0, 1.0, 0.0]], lcar
+    )
+
+    top, volume, lat = geom.extrude(poly.surface, [0, 0, 2])
+
+    geom.add_physical(poly.surface, label="bottom")
+    geom.add_physical(top, label="top")
+    geom.add_physical(volume, label="volume")
+    geom.add_physical(lat, label="lat")
+    geom.add_physical(poly.lines[0], label="line")
+
+    mesh = pygmsh.generate_mesh(geom, geo_filename="physical.geo")
+    return mesh
+
+
+if __name__ == "__main__":
+    import meshio
+
+    meshio.write("physical.vtu", test())
