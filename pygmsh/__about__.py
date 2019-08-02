b00879dab9d8cdb234babb5bd1d444be180b8336
Nico Schlömer <nico.schloemer@gmail.com>
2019-08-02 16:55:03 +0200

    major version bump

--- pygmsh/__about__.py
+++ pygmsh/__about__.py
@@ -1,4 +1,4 @@
-__version__ = "5.0.2"
+__version__ = "6.0.2"
 __author__ = "Nico Schlömer"
 __author_email__ = "nico.schloemer@gmail.com"
 __copyright__ = "Copyright (c) 2013-2019, {} <{}>".format(__author__, __author_email__)
