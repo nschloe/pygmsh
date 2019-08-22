dc583b90f40f4717b1d186f6ee6a7cfa53a710e2
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2019-08-22 15:47:53 +0200

    Inherit ellisoid from ball
    This way we inherit also all the cutting options Ball has.

--- pygmsh/opencascade/ellipsoid.py
+++ pygmsh/opencascade/ellipsoid.py
@@ -1,7 +1,7 @@
-from .volume_base import VolumeBase
+from .ball import Ball
 
 
-class Ellipsoid(VolumeBase):
+class Ellipsoid(Ball):
     """
     Creates an ellipsoid.
 
@@ -11,24 +11,33 @@ class Ellipsoid(VolumeBase):
         Center of the ellipsoid.
     radii: array-like[3]
         The radii of the ellipsoid along the semi-axes
+    x0: float
+        If specified and `x0 > -1`, the ellipsoid is cut off at `x0*radius`
+        parallel to the y-z plane.
+    x1: float
+        If specified and `x1 < +1`, the ellipsoid is cut off at `x1*radius`
+        parallel to the y-z plane.
+    alpha: float
+        If specified and `alpha < 2*pi`, the points between `alpha` and
+        `2*pi` w.r.t. to the x-y plane are not part of the object.
     char_length: float
         If specified, sets the `Characteristic Length` property.
     """
 
-    def __init__(self, center, radii, char_length=None):
-        super(Ellipsoid, self).__init__()
-
+    def __init__(self, center, radii, char_length=None, **kwargs):
         self.center = center
         self.radii = radii
         self.char_length = char_length
+
+        # Instantiate the ball and later stretch it
+        Ball.__init__(self, center, 1.0, char_length=None, **kwargs)
 
         def as_str(point):
             return ", ".join(str(x) for x in point)
 
         self.code = "\n".join(
             [
-                "{} = newv;".format(self.id),
-                "Sphere({}) = {{ {}, 1.0 }};".format(self.id, as_str(center)),
+                self.code,
                 "Dilate {{{{{}}}, {{{}}}}} {{ Volume{{{}}}; }}".format(
                     as_str(center), as_str(radii), self.id
                 ),
