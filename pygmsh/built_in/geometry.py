61882d9a233dd9deac9e517a58653877435d43db
awa5114 <amine.aboufirass@gmail.com>
2020-02-13 13:25:07 +0100

    Adding function to recombine mesh for generation of quad elements (#305)
    * adding set_recombined_surfaces in geometry.py for generation of quad meshes and adding corresponding test in test_recombine.py. Related to issue #304
    
    * reformatting test_recombine.py to pass ci/lint checks:
    
    *  adding whitespace around arithmetic operator as indicated by ci/lint tests
    
    * using black to reformat
    
    * reformatting test_recombine.py

--- pygmsh/built_in/geometry.py
+++ pygmsh/built_in/geometry.py
@@ -258,6 +258,17 @@ class Geometry:
         self._GMSH_CODE.append(code + ";")
         return
 
+    def set_recombined_surfaces(self, surfaces):
+        for i, surface in enumerate(surfaces):
+            assert isinstance(
+                surface, (PlaneSurface, Surface)
+            ), "item {} is not a surface".format(i)
+        code = "Recombine Surface {{{}}}".format(
+            ", ".join([surface.id for surface in surfaces])
+        )
+        self._GMSH_CODE.append(code + ";")
+        return
+
     def add_circle(
         self,
         x0,
