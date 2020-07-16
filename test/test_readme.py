6f4ed2971e99d8e80f1ca2c182be5d2c6e83213e
Nico Schlömer <nico.schloemer@gmail.com>
2020-07-16 21:11:42 +0200

    fix for new exdown

--- test/test_readme.py
+++ test/test_readme.py
@@ -7,7 +7,8 @@ this_dir = pathlib.Path(__file__).resolve().parent
 
 
 @pytest.mark.parametrize(
-    "string", exdown.extract(this_dir.parent / "README.md", syntax_filter="python"),
+    "string, lineno",
+    exdown.extract(this_dir.parent / "README.md", syntax_filter="python"),
 )
-def test_readme(string):
+def test_readme(string, lineno):
     exec(string)
