166da2bdc714f1a10f043fe807c1d61f2ffe1ac4
Nico Schlömer <nico.schloemer@gmail.com>
2020-10-07 10:58:30 +0200

    extract physical data as cell sets

--- pygmsh/common/geometry.py
+++ pygmsh/common/geometry.py
@@ -401,26 +401,37 @@ class CommonGeometry:
                 )
             )
 
-        print(gmsh.model.getPhysicalGroups())
-        for dim_tag in gmsh.model.getPhysicalGroups():
-            name = gmsh.model.getPhysicalName(*dim_tag)
-            print(name)
-            ent = gmsh.model.getEntitiesForPhysicalGroup(*dim_tag)
-            print(ent)
-        exit(1)
+        cell_sets = {}
+        for dim, tag in gmsh.model.getPhysicalGroups():
+            name = gmsh.model.getPhysicalName(dim, tag)
+            cell_sets[name] = [[] for _ in range(len(cells))]
+            for e in gmsh.model.getEntitiesForPhysicalGroup(dim, tag):
+                # TODO node_tags?
+                # elem_types, elem_tags, node_tags
+                elem_types, elem_tags, _ = gmsh.model.mesh.getElements(dim, e)
+                assert len(elem_types) == len(elem_tags)
+                assert len(elem_types) == 1
+                elem_type = elem_types[0]
+                elem_tags = elem_tags[0]
 
-        # print("a", gmsh.model.getEntities())
-        # grps = gmsh.model.getPhysicalGroups()
-        # print("a", grps)
-        # for dim, tag in grps:
-        #     print("a", gmsh.model.getPhysicalName(dim, tag))
-        #     ent = gmsh.model.getEntitiesForPhysicalGroup(dim, tag)
-        #     print("a", ent)
-        #     assert len(ent) == 1
-        #     print("a", gmsh.model.mesh.getElements(dim, ent[0]))
+                meshio_cell_type = meshio.gmsh.gmsh_to_meshio_type[elem_type]
+                # make sure that the cell type appears only once in the cell list
+                # -- for now
+                idx = []
+                for k, cell_block in enumerate(cells):
+                    if cell_block.type == meshio_cell_type:
+                        idx.append(k)
+                assert len(idx) == 1
+                idx = idx[0]
+                cell_sets[name][idx].append(elem_tags - 1)
+
+            cell_sets[name] = [
+                (None if len(idcs) == 0 else numpy.concatenate(idcs))
+                for idcs in cell_sets[name]
+            ]
 
         # make meshio mesh
-        mesh = meshio.Mesh(points, cells)
+        mesh = meshio.Mesh(points, cells, cell_sets=cell_sets)
 
         if remove_lower_dim_cells:
             # Only keep the cells of highest topological dimension; discard faces and
