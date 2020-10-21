634bcb52f7817eca2488a3321ad4317e8e63b072
Nico Schlömer <nico.schloemer@gmail.com>
2020-10-21 12:42:01 +0200

    add missing file

---
+++ pygmsh/geo/dummy.py
@@ -0,0 +1,10 @@
+class Dummy:
+    def __init__(self, dim, id0):
+        assert isinstance(id0, int)
+        self.dim = dim
+        self._ID = id0
+        self.dim_tag = (dim, id0)
+        self.dim_tags = [self.dim_tag]
+
+    def __repr__(self):
+        return f"<pygmsh Dummy object, ID {self._ID}>"
