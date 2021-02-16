4845f01df946c99df0e1e7d471fdaedac7fe4cb8
Antonio Cervone <ant.cervone@gmail.com>
2021-02-16 16:35:46 +0100

    fix geom.set_transfinite_surface() usage in test_transfinite

--- test/built_in/test_transfinite.py
+++ test/built_in/test_transfinite.py
@@ -1,14 +1,14 @@
 import pygmsh
 
 
-def test(lcar=1.0):
+def test(lcar=0.1):
     with pygmsh.geo.Geometry() as geom:
         poly = geom.add_polygon(
             [[0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [1.0, 1.0, 0.0], [0.0, 1.0, 0.0]], lcar
         )
-        geom.set_transfinite_surface(poly, "Left", corner_tags=[])
+        geom.set_transfinite_surface(poly, "Left", corner_pts=[])
         mesh = geom.generate_mesh()
-    # assert len(mesh.cells_dict["triangle"]) == 10 * 8 * 2
+    assert len(mesh.cells_dict["triangle"]) == 10 * 10 * 2
     return mesh
 
 
