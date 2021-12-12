d00bbee5e3f430bd77bff1dcbd0604b7fa6aa9db
arpit agarwal <arpit15945@gmail.com>
2021-12-11 20:30:26 -0500

    Add support for plane spec in circle arc

--- src/pygmsh/common/circle_arc.py
+++ src/pygmsh/common/circle_arc.py
@@ -13,9 +13,9 @@ class CircleArc(LineBase):
     end : Coordinates of end point needed to construct circle-arc.
     """
 
-    def __init__(self, env, start: Point, center: Point, end: Point):
+    def __init__(self, env, start: Point, center: Point, end: Point, nx: float = 0., ny: float = 0., nz: float = 0.):
         assert isinstance(start, Point)
         assert isinstance(center, Point)
         assert isinstance(end, Point)
-        id0 = env.addCircleArc(start._id, center._id, end._id)
+        id0 = env.addCircleArc(start._id, center._id, end._id, nx=nx, ny=ny, nz=nz)
         super().__init__(id0, [start, center, end])
